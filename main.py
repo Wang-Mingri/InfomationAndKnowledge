c5baf9fcc8183f74a858c816dbf0503d405cfd5c
Arete <KakiaArete0@163.com>
2022-06-03 00:35:17 +0800

    小修改

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os.path, time
 
 import spider
 from InformationRetrieval.buildindex import *
@@ -7,11 +7,19 @@ from InformationRetrieval.getindex import *
 
 if __name__ == '__main__':
     # 若数据未爬取或者爬取文档数目小于100 重新爬取
-    if not (os.path.exists('/data/') | len(os.listdir('/data/')) > 100):
+    if not (os.path.exists('data/') | len(os.listdir('data/')) > 100):
         spider()
 
-    # 创建倒叙索引表
-    buildIndex()
+    # 若不存在倒叙索引表 或者 创建时间早于data 则创建倒叙索引表
+    if not (os.path.exists('index.json') | os.path.exists('wordlist.json')):
+        buildIndex()
+    else:
+        index_time = time.localtime(os.stat("wordlist.json").st_mtime)
+        data_time = time.localtime(os.stat(f"data/{os.listdir('data/')[0]}").st_mtime)
+        if index_time < data_time:
+            buildIndex()
 
+    index = getIndex()
+    worl_list = getWordList()
 
 
