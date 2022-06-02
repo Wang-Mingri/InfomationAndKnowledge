e572ceadc4fdbe2c13576421d4f38c9d3a614397
Wang-Mingri <56691723+Wang-Mingri@users.noreply.github.com>
2022-06-03 00:14:25 +0800

    refresh

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from InformationRetrieval.getindex import *
 
 if __name__ == '__main__':
     # 若数据未爬取或者爬取文档数目小于100 重新爬取
-    if not (os.path.exists('/data/') | len(os.listdir('/data/')) > 100):
+    if not (os.path.exists('data/') | len(os.listdir('data/')) > 100):
         spider()
 
     # 创建倒叙索引表
