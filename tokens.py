b396e389c5a743e2d85211b945a141855746a3de
Wang-Mingri <56691723+Wang-Mingri@users.noreply.github.com>
2022-06-02 23:14:13 +0800

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,6 +1,6 @@
 # encoding=utf-8
 import json
-# import jieba
+import jieba
 import stanza
 import re
 import os
@@ -9,41 +9,44 @@ import os
 def findAllFile(base):
     for root, ds, fs in os.walk(base):
         for f in fs:
-            if f.endswith('data/2022-03-02-07-59.json'):
+            if f.endswith('.json'):
                 fullname = os.path.join(root, f)
                 yield fullname
 
 
-def test():
+def getToken():
     data_folder = "./data"
-    stanza.download('zh')
-    zh_nlp = stanza.Pipeline('zh', use_gpu=False)
+    # stanza.download('zh')
+    # zh_nlp = stanza.Pipeline('zh', use_gpu=False)
+    folder = []
 
     for file_name in findAllFile(data_folder):
         # file_name = "data/2022-03-02-07-59.json"
         text = json.loads(open(file_name, 'r').read())
         # print(*text)
+        result = []
 
         attribute = "正文"
         attribute_text = text[attribute]
-        attribute_text = re.sub('[“”‘’、()]', '', attribute_text)
+        attribute_text = re.sub('[“”‘’、() ]', '', attribute_text)
         # print(attribute_text)
         segment_list = re.split('[，,。：；\n\r\t]', attribute_text)
         while '' in segment_list:
             segment_list.remove('')
-        print(*segment_list)
+        # print(*segment_list)
 
         for segment in segment_list:
             # print(segment)
-            # token_list = jieba.cut(segment, use_paddle=True)
-            token_list = zh_nlp(segment)
-            print(*token_list)
-            # print("Default Mode: " + "/ ".join(seg_list))  # 精确模式
-
+            token_list = jieba.cut(segment, use_paddle=True)
+            # token_list = zh_nlp(segment)
+            # print(*token_list)
+            result += token_list
+        # print(*result)
+        folder.append(result)
+    return folder
 
 if __name__ == "__main__":
     # diction_path = "./diction.txt"
     # jieba.load_userdict(diction_path)
-
-    test()
+    getToken()
 
