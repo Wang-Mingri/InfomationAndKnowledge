3f8c5da72cf36faa98e79122e8bc1f1e531c2076
Wang-Mingri <56691723+Wang-Mingri@users.noreply.github.com>
2022-06-02 23:30:42 +0800

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,52 +1,46 @@
 # encoding=utf-8
+
 import json
 import jieba
-import stanza
 import re
-import os
+import logging
+# import stanza
 
 
-def findAllFile(base):
-    for root, ds, fs in os.walk(base):
-        for f in fs:
-            if f.endswith('.json'):
-                fullname = os.path.join(root, f)
-                yield fullname
+diction_path = "./diction.txt"
+jieba.load_userdict(diction_path)
+
+jieba.setLogLevel(logging.INFO)
 
 
-def getToken():
-    data_folder = "./data"
-    # stanza.download('zh')
-    # zh_nlp = stanza.Pipeline('zh', use_gpu=False)
-    folder = []
+def getToken(single_name):
 
-    for file_name in findAllFile(data_folder):
-        # file_name = "data/2022-03-02-07-59.json"
-        text = json.loads(open(file_name, 'r').read())
-        # print(*text)
-        result = []
+    file_name = "data/" + single_name
 
-        attribute = "正文"
-        attribute_text = text[attribute]
-        attribute_text = re.sub('[“”‘’、() ]', '', attribute_text)
-        # print(attribute_text)
-        segment_list = re.split('[，,。：；\n\r\t]', attribute_text)
-        while '' in segment_list:
-            segment_list.remove('')
-        # print(*segment_list)
+    # file_name = "data/2022-03-02-07-59.json"
+    text = json.loads(open(file_name, 'r').read())
+    # print(*text)
+    result = []
 
-        for segment in segment_list:
-            # print(segment)
-            token_list = jieba.cut(segment, use_paddle=True)
-            # token_list = zh_nlp(segment)
-            # print(*token_list)
-            result += token_list
-        # print(*result)
-        folder.append(result)
-    return folder
+    attribute = "正文"
+    attribute_text = text[attribute]
+    attribute_text = re.sub('[“”‘’、()《》 ]', '', attribute_text)
+    # print(attribute_text)
+    segment_list = re.split('[，,。：；\n\r\t]', attribute_text)
+    while '' in segment_list:
+        segment_list.remove('')
+    # print(*segment_list)
+
+    for segment in segment_list:
+        # print(segment)
+        token_list = jieba.cut(segment, use_paddle=True)
+        # token_list = zh_nlp(segment)
+        # print(*token_list)
+        result += token_list
+    print(*result)
+    return result
+
 
 if __name__ == "__main__":
-    # diction_path = "./diction.txt"
-    # jieba.load_userdict(diction_path)
-    getToken()
+    getToken("2022-03-01-10-04.json")
 
