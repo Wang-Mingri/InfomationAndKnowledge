5c2dd3e89350b391cb62cf1178b468baf960d010
Wang-Mingri <56691723+Wang-Mingri@users.noreply.github.com>
2022-06-04 15:15:43 +0800

    final

--- InformationRetrieval/VSM.py
+++ InformationRetrieval/VSM.py
@@ -63,10 +63,15 @@ def printResult(index, reports):
 
         content = list(tokens.getToken(file_name))
         i = 0
-        while i != len(content):
+        output_len = 0
+        while i != len(content) and output_len <= 100:
             if i in report[0][1]:
+                if output_len == 0 and i != 0: print('...', end='')
                 print(f'\033[91m{content[i]}\033[0m', end='')
-            else:
+                output_len += len(content[i])
+            elif output_len > 0:
                 print(content[i], end='')
+                output_len += len(content[i])
             i += 1
+        if i != len(content): print('...', end='')
         print()
