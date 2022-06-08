f149d0d0a74e5f1629e5cdf16a9a06f89a15842d
Wang-Mingri <56691723+Wang-Mingri@users.noreply.github.com>
2022-06-08 16:25:24 +0800

    10 - 23 json

--- IE/main.py
+++ IE/main.py
@@ -8,15 +8,18 @@ def test():
 
     HanLP = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_BASE_ZH)
 
-    text = json.loads(open('data/12_1.json', 'r').read())
-    doc = HanLP([text["文本"]])
+    text = json.loads(open('data/13_2.json', 'r').read())
+    doc = HanLP(text["标题"], tasks='srl')
+    print(doc["srl"][0][2][0])
+    doc = HanLP(doc["srl"][0][2][0])
     print(doc)
-    file = open("1text.txt", 'w')
-    file.write(str(doc))
-    file.close()
+
+    # file = open("output.txt", 'w')
+    # file.write(str(doc))
+    # file.close()
 
 if __name__ == '__main__':
     # 若数据未爬取或者爬取文档数目小于100 重新爬取
-    if not (os.path.exists('data/') | len(os.listdir('data/')) > 100):
-        spider()
+    # if not (os.path.exists('data/') | len(os.listdir('data/')) > 100):
+    #     spider()
     test()
