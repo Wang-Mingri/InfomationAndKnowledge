2dc000df12ec4254bf075356114c00f0c456e372
Arete <KakiaArete0@163.com>
2022-06-08 16:36:30 +0800

    TODO

--- IE/main.py
+++ IE/main.py
@@ -2,7 +2,8 @@ import hanlp
 import json
 import os
 from Spider.spider import *
-
+from InformationExtraction.participle import *
+from InformationExtraction.regularmatch import *
 def test():
     hanlp.pretrained.mtl.ALL # MTL多任务，具体任务见模型名称，语种见名称最后一个字段或相应语料库
 
@@ -19,4 +20,20 @@ if __name__ == '__main__':
     # 若数据未爬取或者爬取文档数目小于100 重新爬取
     if not (os.path.exists('data/') | len(os.listdir('data/')) > 100):
         spider()
-    test()
+    # TODO 分词输出到 json文件
+    for i in range (len(os.listdir('data/'))):
+        participle(filename)
+
+    # TODO 从json文件中获取数据 正则匹配 输出到 result 格式如第三个todo
+    for i in range(len(os.listdir('json文件/'))):
+        regularmatch(filename)
+
+    # TODO 输出result
+    # json文件filename：？？？if or not
+    # 留言关键词：
+    # 留言人地区：
+    # 留言时间：
+    # 政府部门类别：
+    # 相关法律法规：
+    # 具体解决方法：
+
