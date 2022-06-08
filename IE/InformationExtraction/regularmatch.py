22a39e63aab39a97a0b9bbe482817eca99b95402
Arete <KakiaArete0@163.com>
2022-06-08 18:33:57 +0800

    正则匹配 法律，人名， 手机尾号

--- IE/InformationExtraction/regularmatch.py
+++ IE/InformationExtraction/regularmatch.py
@@ -1,3 +1,57 @@
 import re
+import json
+
+
+def cut_sentences(content):
+    sentences = re.split(r'(\!|\?|。|！|？|：)', content)
+    return sentences
+
 
 def regularmatch(filename):
+    data = json.load(open(f"data/{filename}", "r"));
+    content = cut_sentences(data["文本"]) # 对文本分句
+    dir = {}
+    dir["网名"] = []
+    dir["相关法律"] = []
+    dir["手机尾号"] = []
+    for i in content:
+        if re.match(r'.*网民“(.*)”.*', i, re.M | re.I):
+            dir["网名"].append(re.match(r'.*网民“(.*)”.*', i, re.M | re.I).group(1)) # 获取 网名
+        if re.match( r'.*(《.*》).*', i, re.M|re.I):
+            dir["相关法律"].append(re.match( r'.*(《.*》).*', i, re.M|re.I).group(1)) # 获取 相关法律
+        if re.match(r'.*手机尾号(.*)）.*', i, re.M | re.I):
+            dir["手机尾号"].append(re.match(r'.*手机尾号(.*)）.*', i, re.M | re.I).group(1)) # 获取手机尾号后四位
+
+    return dir
+
+
+
+
+# 获取 相关法律
+# line = "国务院办公厅印发《推进运输结构调整三年行动计划（2018—2020年）》"
+# matchObj = re.match( r'.*(《.*》).*', line, re.M|re.I)
+# if matchObj:
+#     print ("matchObj.group() : ", matchObj.group())
+#     print ("matchObj.group(1) : ", matchObj.group(1))
+# else:
+#     print ("No match!!")
+
+
+# 获取 网名
+# line = "来自江苏苏州的网民“华庆春”（手机尾号2715）说,来自湖北恩施的网民“小朵”（手机尾号5782）说"
+# matchObj = re.match( r'.*网民“(.*)”.*', line, re.M|re.I)
+# if matchObj:
+#     print ("matchObj.group() : ", matchObj.group())
+#     print ("matchObj.group(1) : ", matchObj.group(1))
+# else:
+#     print ("No match!!")
+
+
+# 获取手机尾号后四位
+# line = "来自江苏苏州的网民“华庆春”（手机尾号2715）说,来自湖北恩施的网民“小朵”（手机尾号5782）说"
+# matchObj = re.match(r'.*手机尾号(.*)）.*', line, re.M | re.I)
+# if matchObj:
+#     print("matchObj.group() : ", matchObj.group())
+#     print("matchObj.group(1) : ", matchObj.group(1))
+# else:
+#     print("No match!!")
--- IE/main.py
+++ IE/main.py
@@ -23,20 +23,23 @@ if __name__ == '__main__':
     # 若数据未爬取或者爬取文档数目小于100 重新爬取
     if not (os.path.exists('data/') | len(os.listdir('data/')) > 100):
         spider()
-    # TODO 分词输出到 json文件
-    for i in range (len(os.listdir('data/'))):
-        participle(filename)
+    for i in os.listdir('data/'):
+        regularmatch(i)
 
-    # TODO 从json文件中获取数据 正则匹配 输出到 result 格式如第三个todo
-    for i in range(len(os.listdir('json文件/'))):
-        regularmatch(filename)
-
-    # TODO 输出result
-    # json文件filename：？？？if or not
-    # 留言关键词：
-    # 留言人地区：
-    # 留言时间：
-    # 政府部门类别：
-    # 相关法律法规：
-    # 具体解决方法：
+    # # TODO 分词输出到 json文件
+    # for i in range (len(os.listdir('data/'))):
+    #     participle(filename)
+    #
+    # # TODO 从json文件中获取数据 正则匹配 输出到 result 格式如第三个todo
+    # for i in range(len(os.listdir('json文件/'))):
+    #     regularmatch(filename)
+    #
+    # # TODO 输出result
+    # # json文件filename：？？？if or not
+    # # 留言关键词：
+    # # 留言人地区：
+    # # 留言时间：
+    # # 政府部门类别：
+    # # 相关法律法规：
+    # # 具体解决方法：
 
