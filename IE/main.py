36269fe49f9762feb1682730840ad46cc2e076e0
Wang-Mingri <56691723+Wang-Mingri@users.noreply.github.com>
2022-06-08 22:58:45 +0800

    merge一下

--- IE/main.py
+++ IE/main.py
@@ -4,6 +4,51 @@ from Spider.spider import *
 from InformationExtraction.participle import *
 from InformationExtraction.regularmatch import *
 
+HanLP = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_BASE_ZH)
+
+
+def get_value(origin_list, label):
+    token_list = origin_list["ner/msra"]
+    result_str = ""
+    for token in token_list:
+        if token[1] == label:
+            result_str += token[0]
+    return result_str
+
+
+def getKeywordsFromHanlp(filename):
+    text = json.loads(open(filename, 'r').read())
+    organization = get_value(HanLP(text["标题"], tasks="ner/msra"), 'ORGANIZATION')
+    location_segment = text["文本"].split('：')[0]
+    location = get_value(HanLP(location_segment, tasks="ner/msra"), 'LOCATION')
+    time = text["时间"]
+    strong = []
+    try:
+        strong = text["强调文本"]
+    except:
+        pass
+    print(f"Processing Page Section", end='\n\n')
+    return [organization, location, time, strong]
+
+
+def output(json_file):
+    try:
+        dict = json.loads(open(json_file, 'r').read())
+        print("信息点抽取结果如下:", end='\n\n')
+        for key, value in dict.items():
+            value_str = ''
+            if isinstance(value, list):
+                value_str = '\n'.join(value)
+            elif isinstance(value, str):
+                value_str = value
+
+            if len(value_str) != 0:
+                print('{a}:{b}'.format(a=key, b=value_str))
+        print("输出完毕。", end='\n\n')
+    except FileNotFoundError:
+        print(f"未找到目标文件: json_file")
+
+
 if __name__ == '__main__':
     # 若数据未爬取或者爬取文档数目小于100 重新爬取
     if not (os.path.exists('data/') | len(os.listdir('data/')) > 100):
@@ -17,19 +62,8 @@ if __name__ == '__main__':
             json.dump(dict, write_f, indent=4, ensure_ascii=False)
 
 
-    # # TODO 分词输出到 json文件
-    # for i in range (len(os.listdir('data/'))):
-    #     participle(filename)
-    #
-    # # TODO 从json文件中获取数据 正则匹配 输出到 result 格式如第三个todo
-    # for i in range(len(os.listdir('json文件/'))):
-    #     regularmatch(filename)
-    #
-    # # TODO 输出result
-    # # json文件filename：？？？if or not
-    # # 留言关键词：
-    # # 留言人地区：
-    # # 留言时间：
-    # # 政府部门类别：
-    # # 相关法律法规：
-    # # 具体解决方法：
+    print("************************************信息抽取系统************************************")
+    while True:
+        page = input("请输入待抽取文本所在页码(回车代表对所有页面进行查询): ")
+        segment = input("请输入指定页面上的待抽取文件所在位置(回车代表对指定页面所有项目进行查询): ")
+        output(f'data/{page}_{segment}.json')
